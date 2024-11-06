d70c2852e8b56bd7f41b0b50ceba0053f68efc54
Hug Folguera <hfolguera@gmail.com>
2024-11-06 12:42:59 +0100

    Fix backup script

--- backup.sh
+++ backup.sh
@@ -13,12 +13,25 @@ NS=mysql
 POD=`kubectl get pod -n $NS -o jsonpath='{.items[0].metadata.name}'`
 echo "[DEBUG] POD: $POD"
 
-DATABASES=`kubectl exec -it -n mysql $POD -- mysql -u root --password=${PASS} -e "SHOW DATABASES;" | grep -v "+-" | grep -v Database | grep -v information_schema | grep -v performance_schema | grep -v Warning | awk '{print $2}'`
+DATABASES=`kubectl exec -i -n $NS $POD -- /usr/bin/mysql -u root --password=${PASS} -e "SHOW DATABASES;" | grep -v "+-" | grep -v Database | grep -v information_schema | grep -v performance_schema | grep -v Warning | awk '{print $1}'`
+if [ $? -eq 0 ]
+then
+    echo "[DEBUG] DATABASES: $DATABASES"
+else
+    echo "[ERROR] Cannot get MySQL databases!"
+    exit 1
+fi
 
+RC=0
 for DB in $DATABASES
 do
     echo "[INFO] Executing backup for $DB"
-    kubectl exec -it -n mysql $POD -- /usr/bin/mysqldump -u root --password=${PASS} $DB > $DEST_PATH/mysql_${DB}_backup_${DATE}.sql
+    kubectl exec -it -n $NS $POD -- /usr/bin/mysqldump -u root --password=${PASS} $DB > $DEST_PATH/mysql_${DB}_backup_${DATE}.sql
+    if [ $? -ne 0 ]
+    then
+        echo "[ERROR] Cannot execute $DB database backup!"
+        RC=1
+    fi
 done
 
 echo "[INFO] Backup finished"
@@ -27,3 +40,5 @@ echo "[INFO] Purging old backups"
 find $DEST_PATH/*.sql -mtime +7 -exec rm {} \;
 echo "[INFO] End of script"
 echo ""
+
+exit $RC
